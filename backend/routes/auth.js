1b349951729f158535638113f69f654374c41bb3
ansh <ianshpwr@gmail.com>
2025-11-17 22:51:24 +0530

    added consoles

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -27,7 +27,7 @@ router.post("/signup", async (req,res)=>{
             user : user
         })
     }catch(err){
-        res.status(500).json({message : "Error creating user"})
+        res.status(500).json({message : "Error creating user",issue : err.message})
     }
 })
 
